e4f91bf4b4c787735ea09ed45e7ac8f44823a040
Harshit Jadon <harshit.jadon@magicedtech.com>
2023-03-25 19:10:12 +0530

    get all books from api

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import "./Dashboard.css";
 import Header from "../header/Header";
-import * as BookApi from "../BookApi"
+import * as BookApi from "../BookApi";
+import { useEffect, useState } from "react";
 
 const Dashboard = (props) => {
+  const [allBooks, setAllBooks] = useState([]);
 
+  const getAllBooks = () => {
+    const getBooks = async () => {
+      const resp = await BookApi.getAll();
+      console.log("resp >>", resp);
+      setAllBooks(resp);
+    };
+    getBooks();
+  };
+
+  useEffect(() => {
+    getAllBooks();
+  }, []);
 
   return (
     <div>
@@ -12,192 +26,116 @@ const Dashboard = (props) => {
       <div>
         <div>
           <h2 className="heading-border">Currently Reading</h2>
-          <div className="book-section">
-            <div>
-              <div className="book-section-main">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                  }}
-                ></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to.. </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none"> None</option>
-                  </select>
+          <div className="book-main-section">
+          {allBooks
+            .filter((ele) => ele.shelf === "currentlyReading")
+            .map((element) => (
+              <div className="book-section" key={element.id}>
+                <div className="book-section-main">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${element.imageLinks.thumbnail})`,
+                    }}
+                  ></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>
+                        Move to..{" "}
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none"> None</option>
+                    </select>
+                  </div>
                 </div>
+                <div className="book-name">{element.title}</div>
+                <div className="author-name">{element.authors[0]}</div>
               </div>
-              <div className="book-name">Commerce Book</div>
-              <div className="author-name">Tanisha jadon</div>
+            ))}
             </div>
-
-            <div>
-              <div className="book-section-main">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                  }}
-                ></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to.. </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none"> None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-name">Commerce Book</div>
-              <div className="author-name">Tanisha jadon</div>
-            </div>
-
-            <div>
-              <div className="book-section-main">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                  }}
-                ></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to.. </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none"> None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-name">Commerce Book</div>
-              <div className="author-name">Tanisha jadon</div>
-            </div>
-
-            <div>
-              <div className="book-section-main">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                  }}
-                ></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to.. </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none"> None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-name">Commerce Book</div>
-              <div className="author-name">Tanisha jadon</div>
-            </div>
-          </div>
         </div>
 
         <div>
           <h2 className="heading-border">Want to Read</h2>
-          <div className="book-section">
-            <div>
-              <div className="book-section-main">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                  }}
-                ></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to.. </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none"> None</option>
-                  </select>
+          <div className="book-main-section">
+          {allBooks
+            .filter((ele) => ele.shelf === "wantToRead")
+            .map((element) => (
+              <div className="book-section" key={element.id}>
+                <div className="book-section-main">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${element.imageLinks.thumbnail})`,
+                    }}
+                  ></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>
+                        Move to..{" "}
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none"> None</option>
+                    </select>
+                  </div>
                 </div>
+                <div className="book-name">{element.title}</div>
+                <div className="author-name">{element.authors[0]}</div>
               </div>
-              <div className="book-name">Commerce Book</div>
-              <div className="author-name">Tanisha jadon</div>
+            ))}
             </div>
-            <div>
-              <div className="book-section-main">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                  }}
-                ></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to.. </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none"> None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-name">Commerce Book</div>
-              <div className="author-name">Tanisha jadon</div>
-            </div>
-          </div>
         </div>
 
         <div>
           <h2 className="heading-border">Read</h2>
-          <div className="book-section">
-            <div>
-              <div className="book-section-main">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                  }}
-                ></div>
-               <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to.. </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none"> None</option>
-                  </select>
+          <div className="book-main-section">
+          {allBooks
+            .filter((ele) => ele.shelf === "read")
+            .map((element) => (
+              <div className="book-section" key={element.id}>
+                <div className="book-section-main">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${element.imageLinks.thumbnail})`,
+                    }}
+                  ></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>
+                        Move to..{" "}
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none"> None</option>
+                    </select>
+                  </div>
                 </div>
+                <div className="book-name">{element.title}</div>
+                <div className="author-name">{element.authors[0]}</div>
               </div>
-              <div className="book-name">Commerce Book</div>
-              <div className="author-name">Tanisha jadon</div>
+            ))}
             </div>
-          </div>
         </div>
+
+
       </div>
       <div className="open-search">
         <Link to="/search">Add a book</Link>
